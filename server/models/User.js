718fda357236272e1abf7dae289ceef8e170a999
Samee Shahood <sameeshahood11@gmail.com>
2021-01-30 15:03:30 -0500

    Created User Model

--- server/models/User.js
+++ server/models/User.js
@@ -1,22 +1,34 @@
 const mongoose = require('mongoose');
 
 const UserSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: true
-  },
-  email: {
-    type: String,
-    required: true
-  },
-  password: {
-    type: String,
-    required: true
-  },
-  date: {
-    type: Date,
-    default: Date.now
-  }
+	name: {
+		type: String,
+		required: true
+	},
+	discordID: {
+		type: String,
+		required: true
+	},
+	partnerID:{
+		type: String,
+		default: ''
+	},
+	github: {
+		type:String,
+		default: ''
+	},
+	spotify: {
+		type: String,
+		default: ''
+	},
+	denylist: {
+		type: [String],
+		default: []
+	},
+	date: {
+		type: Date,
+		default: Date.now
+	}
 });
 
 const User = mongoose.model('User', UserSchema);
