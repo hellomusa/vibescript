b677ffd7271428f40bdecf850207e361bc4d8789
Hamza Sohail <hamza.sohail@carelton.ca>
2021-01-30 14:23:24 -0500

    Add mongo URI

--- server/config/keys.js
+++ server/config/keys.js
@@ -1,4 +1,4 @@
-dbPassword = 'mongodb+srv://YOUR_USERNAME_HERE:'+ encodeURIComponent('YOUR_PASSWORD_HERE') + '@CLUSTER_NAME_HERE.mongodb.net/test?retryWrites=true';
+dbPassword = 'mongodb+srv://dev-user:ciwqyv-9dicdi-xajRuh@dev-cluster.vparc.mongodb.net/dev-db';
 
 module.exports = {
     mongoURI: dbPassword
