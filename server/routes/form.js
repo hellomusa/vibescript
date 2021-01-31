2b7c5a138f1172ffbf1d811d82e775f324d632f4
Hamza Sohail <hamza.sohail@carelton.ca>
2021-01-30 23:14:21 -0500

    Add route for form ID

--- server/routes/form.js
+++ server/routes/form.js
@@ -1,5 +1,13 @@
 const express = require('express');
 const router = express.Router();
+const User = require('../models/User');
+
+router.get("/form/:id", async (req, res) => {
+    let user = await User.findOne({discordID: req.user.id});
+    user.formID = req.params.id;
+    user.save();
+    res.send(200);
+});
 
 router.post("/form/callback", (req, res) => {
   console.log(req.body);
