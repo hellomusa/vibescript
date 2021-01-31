03c9aad1ae129e61a95e3742bc08df82769aa4aa
Hamza Sohail <hamza.sohail@carelton.ca>
2021-01-31 02:08:59 -0500

    Update form route

--- server/routes/form.js
+++ server/routes/form.js
@@ -10,17 +10,35 @@ router.post("/form/:id", async (req, res) => {
 
     let user = await User.findOne({discordID: req.user.id});
     user.responseID = responseID;
-    user.save();
 
     const token = "DVGXbpjTfJbe34rXfhn3EnkRrJYRvLPV9fyTpS2n1NL5";
-    fetch('https://api.typeform.com/forms/GMdc1yP5/responses?' + new URLSearchParams({
+
+    setTimeout(() => {
+      fetch('https://api.typeform.com/forms/GMdc1yP5/responses?' + new URLSearchParams({
       included_response_ids: responseID,
-    }), {
-    method: 'GET',
-    headers: {
-      'authorization': `bearer ${token}`
+      }), {
+            method: 'GET',
+            headers: {
+            'authorization': `bearer ${token}`
     }}).then(res => res.json())
-    .then(resJson => console.log(resJson));
+    .then(resJson => {
+      const responses = resJson.items[0].answers;
+      responses.forEach((elem) => {
+        if (elem.type == 'text') {
+          user.name = elem.text;
+        }
+        if (elem.type ==  'url') {
+          user.github = elem.url;
+        }
+        if (elem.type == 'choices') {
+          user.genres = elem.choices.labels;
+        }
+      })
+      user.save();
+    });
+    }, 1000)
+
+    
     res.send(200);
 });
 
