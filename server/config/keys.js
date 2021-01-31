3aecee2319b738ac68f1a12a26a3ce4abc971514
Hamza Sohail <hamza.sohail@carelton.ca>
2021-01-31 12:30:57 -0500

    Fix depoy keys

--- server/config/keys.js
+++ server/config/keys.js
@@ -2,5 +2,5 @@ module.exports = {
     mongoURI: 'mongodb+srv://dev-user:ciwqyv-9dicdi-xajRuh@dev-cluster.vparc.mongodb.net/dev-db',
     discordClientID: "805188698253688843",
     discordClientSecret: "wc0S7ecUZLwxhebzU-EfqGqExwwrUGaI",
-    discordCallbackURL: "http://localhost:5000/api/callback"
+    discordCallbackURL: "http://vibescript.com/api/callback"
 };
